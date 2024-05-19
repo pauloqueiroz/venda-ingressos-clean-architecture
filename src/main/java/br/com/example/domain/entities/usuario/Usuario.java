effb1c62303e89ff10b49e49bb64e73d1e4a9298
Paulo Sérgio <pauloqueiroz16@gmail.com>
2024-05-19 17:53:06 -0300

    Adding usuario entity

---
+++ src/main/java/br/com/example/domain/entities/usuario/Usuario.java
@@ -0,0 +1,50 @@
+package br.com.example.domain.entities.usuario;
+
+import java.time.LocalDate;
+
+public class Usuario {
+
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+    private String email;
+
+    public Usuario(String nome, LocalDate nascimento, String email, String cpf) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
