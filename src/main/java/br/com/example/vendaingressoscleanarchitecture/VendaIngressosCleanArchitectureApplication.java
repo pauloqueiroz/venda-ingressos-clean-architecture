b221a1004128bdafaee4d01f334466b4331f40a6
Paulo Sérgio <pauloqueiroz16@gmail.com>
2024-05-19 16:54:06 -0300

    Initial commit with the project created by spring initializr

---
+++ src/main/java/br/com/example/vendaingressoscleanarchitecture/VendaIngressosCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.vendaingressoscleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaIngressosCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaIngressosCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/vendaingressoscleanarchitecture/VendaIngressosCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.vendaingressoscleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaIngressosCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
